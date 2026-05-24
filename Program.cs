80f4ac09daaa2eec2e148f560ad42066eb58312e
Furkan Kerim Karademir <17511208+fkerimk@users.noreply.github.com>
2026-05-24 04:30:11 +0300

    cube

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+internal static class Program {
+
+    private static void Main(string[] args) {
+        
+        SetTraceLogLevel(TraceLogLevel.Error);
+        InitWindow(1280, 720, "RayOptix");
+        SetWindowMonitor(0);
+
+        var cam = new Camera3D {
+
+            Up = Vector3.UnitY,
+            Projection = CameraProjection.Perspective,
+            FovY = 60,
+            Position = new Vector3(5, 5, 5),
+            Target = Vector3.Zero,
+        };
+        
+        while (!WindowShouldClose()) {
+            
+            BeginDrawing();
+            
+            ClearBackground(Color.DarkGray);
+            
+            BeginMode3D(cam);
+            
+            DrawCube(Vector3.Zero, 1, 1, 1, Color.White);
+            
+            EndMode3D();
+            
+            EndDrawing();
+        }
+        
+        CloseWindow();
+    }
+}
